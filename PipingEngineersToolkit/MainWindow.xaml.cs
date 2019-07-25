c774adc8086c7e7b01f4312f40579ba607556cec
Josh <josh799@hotmail.co.uk>
2019-07-25 21:41:49 +0100

    3D render fully working using calculations, need to change camera position

--- PipingEngineersToolkit/MainWindow.xaml.cs
+++ PipingEngineersToolkit/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HelixToolkit.Wpf;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PipingEngineersToolkit
 {
@@ -23,9 +13,37 @@ namespace PipingEngineersToolkit
     public partial class MainWindow : Window
     {
         private double tempChangeNumber, pipeDiaNumber, reqLegLengthNumber, expCoeffNumber, lenChangeNumber;
+        TubeVisual3D origTube = new TubeVisual3D();
+        TubeVisual3D expTube = new TubeVisual3D();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DrawOriginalPipe(Point3D originalStartPoint, Point3D originalEndPoint, double diameter)
+        {
+            origTube.Path = new Point3DCollection();
+            origTube.Path.Add(originalStartPoint);
+            origTube.Path.Add(originalEndPoint);
+            origTube.Diameter = diameter;
+            origTube.Fill = Brushes.Red;
+
+            MainViewPort.Children.Remove(origTube);
+            MainViewPort.Children.Add(origTube);
+        }
+
+        private void DrawExpansionPipe(Point3D originalEndPoint, Point3D cornerPoint, Point3D expansionEndPoint, double diameter)
+        {
+            expTube.Path = new Point3DCollection();
+            expTube.Path.Add(originalEndPoint);
+            expTube.Path.Add(cornerPoint);
+            expTube.Path.Add(expansionEndPoint);
+            expTube.Diameter = diameter;
+            expTube.Fill = Brushes.Blue;
+
+            MainViewPort.Children.Remove(expTube);
+            MainViewPort.Children.Add(expTube);
         }
 
         private void CbExpCoeff_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -123,40 +141,69 @@ namespace PipingEngineersToolkit
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Calculate_Click(object sender, RoutedEventArgs e)
         {
             bool isDesignDouble = Double.TryParse(tbTempDesign.Text, out _);
             bool isAmbDouble = Double.TryParse(tbTempAmb.Text, out _);
-            if (isDesignDouble)
-            {
-                if (isAmbDouble)
-                {
-                    tempChangeNumber = Convert.ToDouble(tbTempDesign.Text) - Convert.ToDouble(tbTempAmb.Text);
-                    tbTempChange.Text = tempChangeNumber.ToString();
-                }
-                else
-                {
-                    // These could be red messages next to the box
-                    MessageBox.Show("T2 - Amb is invalid");
-                }
-            }
-            else
-            {
-                MessageBox.Show("T1 - Design is invalid");
-            }
-
             bool isLenDouble = Double.TryParse(tbLen.Text, out _);
-            if (isLenDouble)
+
+            if (isDesignDouble && isAmbDouble && isLenDouble == true)
             {
+                tempChangeNumber = Convert.ToDouble(tbTempDesign.Text) - Convert.ToDouble(tbTempAmb.Text);
+                tbTempChange.Text = tempChangeNumber.ToString();
+
                 lenChangeNumber = Convert.ToDouble(tbLen.Text) * expCoeffNumber * Convert.ToDouble(tbTempChange.Text);
                 tbLenChange.Text = Convert.ToString(lenChangeNumber);
                 reqLegLengthNumber = 66 * Math.Sqrt(pipeDiaNumber * Convert.ToDouble(tbLenChange.Text));
                 tbReqLegLen.Text = Convert.ToString(Math.Round(reqLegLengthNumber));
+
+                // Pipe Drawing
+                DataContext = this;
+
+                double x2 = Convert.ToDouble(tbLen.Text);
+                double x3 = Convert.ToDouble(tbLen.Text) + Convert.ToDouble(tbLenChange.Text);
+                double y4 = Convert.ToDouble(tbReqLegLen.Text);
+                var pt1 = new Point3D(0, 0, 0);
+                var pt2 = new Point3D(x2, 0, 0);
+                var pt3 = new Point3D(x3, 0, 0);
+                var pt4 = new Point3D(x3, y4, 0);
+                DrawOriginalPipe(pt1, pt2, pipeDiaNumber);
+                DrawExpansionPipe(pt2, pt3, pt4, pipeDiaNumber);
+                // Camera Control
+                //var lookAtPoint = new Point3D(x3 / 2, y4 / 2, 0);
+                //MainViewPort.Camera.LookAt(lookAtPoint, 0);
+                MainViewPort.Camera.Position = new Point3D(x3 / 2, y4 / 2, 0);
+                MainViewPort.Camera.LookDirection = new Vector3D();
+                MainViewPort.Camera.UpDirection = new Vector3D(0, 0, 0);
             }
-            else
+            else if (isDesignDouble == false)
+            {
+                MessageBox.Show("T1 - Design is invalid");
+            }
+            else if (isAmbDouble == false)
+            {
+                // These could be red messages next to the box
+                MessageBox.Show("T2 - Amb is invalid");
+            }
+            else if (isLenDouble == false)
             {
                 MessageBox.Show("Length is invalid");
             }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            tbTempDesign.Text = null;
+            tbTempAmb.Text = null;
+            cbPipeDia.SelectedIndex = -1;
+            tbLenChange.Text = null;
+            tbLen.Text = null;
+            cbExpCoeff.SelectedIndex = -1;
+            tbTempChange.Text = null;
+            tbReqLegLen.Text = null;
+
+            MainViewPort.Children.Remove(origTube);
+            MainViewPort.Children.Remove(expTube);
         }
     }
 }
